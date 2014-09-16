25fd7b5c4274bc86b5e3b9fd6356b7eb711830b2
pk <pk13610@gmail.com>
2014-09-16 15:28:19 +0800

    change comments order

--- douban.user.js
+++ douban.user.js
@@ -2,63 +2,50 @@
 // @name        douban
 // @namespace   https://github.com/pkhopper/gmScripts/raw/master/douban.user.js
 // @include     http://*.douban.com/*
-// @include     http://*.quora.com/*
-// @include     https://github.com/*
 // @grant       none
-// @version     0.1.1
+// @version     0.1.2
 // ==/UserScript==
 
-var bcolor = "#E8D098";
-var fcolor = "#111111";
-
 // set comments background color
-function douban () {
-    document.body.style.background = bcolor; 
-    var lis = document.body.getElementsByClassName("clearfix comment-item");
-    for (var i = lis.length - 1; i >= 0; i--) {
-        lis[i].style.background = bcolor;
-    }
-    var nav = document.body.getElementsByClassName("nav-wrap");
-    if (nav){
-        nav[0].style.background = bcolor;
-    }
-    // 改变小组推荐的位置
-    article = document.getElementsByClassName("article");
-    if (article) {
-        article = article[0];
-        gb=document.getElementsByClassName("group-board")[0];
-        gt=document.getElementById("group-topics");
-        article.insertBefore(gt, gb);
-        // 话题搜索提前
-        gt.insertBefore(gt.getElementsByClassName("group-topic-search")[0],gt.childNodes[0]);
-    };
-}
-
-
-hostname = document.location.hostname
-
-if (hostname.indexOf('github.com') >= 0) {
+function douban () {   
+    var bcolor = "#E8D098";
+    
+    // 改变页面元素颜色
     document.body.style.background=bcolor; 
-    var nav = document.body.getElementsByClassName("header header-logged-in true");
+    var comment_items = $(".clearfix comment-item");
+    if ( comment_items ){
+        for (i=0; i < comment_items.length; i++) {
+            comment_items[i].style.background=bcolor;
+        }
+    }
+    
+    var nav= document.body.getElementsByClassName("nav-wrap");
     if (nav){
         nav[0].style.background=bcolor;
     }
+    
+    // 小组页面调整
+    gt = document.getElementById("group-topics");
+    if (gt) { 
+        // 改变小组推荐的位置
+        article = document.getElementsByClassName("article");
+        gb = document.getElementsByClassName("group-board");
+        if (article && article.length > 0 && gb && gb.length > 0) {
+            article[0].insertBefore(gt, gb[0]);
+        };
+    	// 话题搜索提前
+        gt.insertBefore(gt.getElementsByClassName("group-topic-search")[0],gt.childNodes[0]);
+    }
+    
+    
+    // comments倒序
+    var cnodes = $("#comments");
+    if (cnodes && cnodes.length > 0){
+        var comments = $("#comments")[$("#comments").length-1];
+        for (i=1; i<comments.children.length; i++) {
+            comments.insertBefore(comments.children[i], comments.children[0]);
+        }
+    }
 }
 
-if (hostname.indexOf('douban.com') >= 0) {
-    douban();
-}
-
-if (hostname.indexOf('quora.com') >= 0) {
-    document.body.style.background=bcolor; 
-}
-
-// if (hostname.indexOf('facebook.com') >= 0)  {
-//     document.body.style.background=bcolor; 
-//     var fb = document.getElementById("contentCol");
-//     if (fb) {
-//         fb.style.background=bcolor;
-//     }
-// }
-
-
+douban();
